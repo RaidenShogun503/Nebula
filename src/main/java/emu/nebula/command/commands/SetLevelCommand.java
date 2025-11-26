170ed055d63b21bdbc55696903ef124e31564f82
Furiri <admin@nguyenhieu.click>
2025-11-27 01:13:00 +0700

    Add Proto Request Change Level (Not Work)

--- src/main/java/emu/nebula/command/commands/SetLevelCommand.java
+++ src/main/java/emu/nebula/command/commands/SetLevelCommand.java
@@ -4,6 +4,9 @@ import emu.nebula.Nebula;
 import emu.nebula.command.Command;
 import emu.nebula.command.CommandArgs;
 import emu.nebula.command.CommandHandler;
+import emu.nebula.net.NetMsgId;
+import emu.nebula.proto.Public.ChangeInfo;
+import emu.nebula.proto.Public.WorldClassUpdate;
 
 @Command(label = "setlevel", permission = "player.setlevel", requireTarget = true, desc = "/setlevel. Set player level")
 public class SetLevelCommand implements CommandHandler {
@@ -11,6 +14,11 @@ public class SetLevelCommand implements CommandHandler {
     @Override
     public String execute(CommandArgs args) {
         int level = Integer.parseInt(args.get(0));
+        args.getTarget().addNextPackage(
+                NetMsgId.world_class_change_notify,
+                WorldClassUpdate.newInstance()
+                        .setCur(level)
+                        .setChange(ChangeInfo.newInstance()));
         args.getTarget().setLevel(level);
         return "Level set to " + level;
     }
