7f5dc574888db3df04c4636957a8b4f48c499d1f
Melledy <121644117+Melledy@users.noreply.github.com>
2025-11-26 19:43:53 -0800

    Update setlevel command

--- src/main/java/emu/nebula/command/commands/SetLevelCommand.java
+++ src/main/java/emu/nebula/command/commands/SetLevelCommand.java
@@ -1,25 +1,41 @@
 package emu.nebula.command.commands;
 
-import emu.nebula.Nebula;
 import emu.nebula.command.Command;
 import emu.nebula.command.CommandArgs;
 import emu.nebula.command.CommandHandler;
+import emu.nebula.data.GameData;
 import emu.nebula.net.NetMsgId;
-import emu.nebula.proto.Public.ChangeInfo;
-import emu.nebula.proto.Public.WorldClassUpdate;
+import emu.nebula.proto.NotifyGm.GmWorldClass;
+import emu.nebula.util.Utils;
 
-@Command(label = "setlevel", permission = "player.setlevel", requireTarget = true, desc = "/setlevel. Set player level")
+@Command(label = "setlevel", aliases = {"level", "l"}, permission = "player.level", requireTarget = true, desc = "/level [level]. Set player level")
 public class SetLevelCommand implements CommandHandler {
 
     @Override
     public String execute(CommandArgs args) {
-        int level = Integer.parseInt(args.get(0));
-        args.getTarget().addNextPackage(
-                NetMsgId.world_class_change_notify,
-                WorldClassUpdate.newInstance()
-                        .setCur(level)
-                        .setChange(ChangeInfo.newInstance()));
-        args.getTarget().setLevel(level);
+        // Get target
+        var target = args.getTarget();
+        
+        // Parse level
+        int level = Utils.parseSafeInt(args.get(0));
+        
+        // Check to make sure world class data exists for this level
+        var data = GameData.getWorldClassDataTable().get(level);
+        if (data == null) {
+            return "The game does not support level " + level;
+        }
+        
+        target.setLevel(level);
+        target.setExp(0);
+        
+        // Update
+        target.addNextPackage(
+            NetMsgId.world_class_number_notify,
+            GmWorldClass.newInstance()
+                .setFinalClass(target.getLevel())
+                .setLastExp(target.getExp())
+        );
+        
         return "Level set to " + level;
     }
 }
